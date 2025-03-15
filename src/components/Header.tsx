38ea08a0f5e991bdab299378f2ae9ffb772a64c8
KIMTAESEONG <aka404365@gmail.com>
2025-03-15 22:06:09 +0900

    feat: 경로에 따른 헤더 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link as Link_ } from "gatsby";
 import styled from "@emotion/styled";
+import { useLocation } from "@reach/router";
 
 const HeaderContainer = styled.header`
   z-index: 1000;
@@ -26,9 +27,22 @@ const Link = styled(Link_)`
 `;
 
 const Header = () => {
+  const location = useLocation();
+
+  let linkTo = "/";
+  let label = "Taeseong Blog";
+
+  if (
+    location.pathname.startsWith("/about") ||
+    location.pathname.startsWith("/detail")
+  ) {
+    label = "Taeseong Resume";
+    linkTo = "/about";
+  }
+
   return (
     <HeaderContainer>
-      <Link to="/">Taeseong Dev Blog</Link>
+      <Link to={linkTo}>{label}</Link>
     </HeaderContainer>
   );
 };
